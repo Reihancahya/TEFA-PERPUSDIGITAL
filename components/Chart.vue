9c9f240b521ad3bfe4035bffa7f577ccfc1905cf
Reihancahya <142280555+Reihancahya@users.noreply.github.com>
2024-05-15 14:01:10 +0700

    Add files via upload

---
+++ components/Chart.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Statistik Pengunjung</h1>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>   
+
+<script stup lang="td">
+import Chart, {type ChartConfiguration, type ChartItem } from 'chat.js/auto';
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'pengunjung',
+        backgroundcolor: 'rgb(38, 92, 181)',
+        bordercolor: 'rgb(255, 99, 132)',
+        data: [20, 10, 5, 2, 20, 50, 77],
+    }]
+}
+
+const config: ChartConfiguration = {
+    type: 'bar',
+    dta: data,
+    options:{}
+}
+
+onMounted(() => {
+    const canvastag = <ChartItem>document.getElemenById('myChart')
+    new Chart(
+        canvasTag,
+        config
+    )
+})
+</script>
+
+<style scoped>
+#myChart {
+    width: 1500px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container-fluid">
+        <div class="row my-5">
+            <div class="col-lg-6">
+                <nuxt-link to="/pengunjung/tambah">
+                    <div class="card 1 bg-pengunjung rounded-5">
+                        <div class="card-body">
+                            <h2>pengunjung</h2>
+                        </div>
+                    </div>
+                </nuxt-link>
+            </div>
+
+            <div class="col-lg-6">
+                <nuxt-link to="/buku">
+                    <div class="card bg-buku rounded-5">
+                        <div class="card-body">
+                            <h2>cari buku</h2>
+                        </div>
+                    </div>
+                </nuxt-link>
+            </div>
+        </div>
+    </div>
+    <h1>STATISTIK</h1>
+    <div class="container-fluid">
+        <div class="row my-5">
+            <div class="col-lg-6">
+                    <div class="card rounded-5">
+                        <div class="card-body">
+                            <h2 class="pengunjung">3 pengunjung</h2>
+                        </div>
+                    </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="card  rounded-5">
+                    <div class="card-body">
+                        <div class="buku">
+                            <h2>180 Buku</h2> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</template>
+
+
+<style scoped>
+.card {
+    height: 250px;
+    color: white;
+    box-shadow: 1px 1px 10px #000000;
+    background-color: rgb(0, 81, 255);
+}
+.card.bg-pengunjung {
+    background-image: url('~/assets/img/bg-home-kunjungan.jpeg');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    color: 
+    0rgb(255, 255, 255);
+    
+}
+.card.bg-buku.rounded-5{
+    background: url('~/assets/img/bg-home-cari-buku.jpg') no-repeat center center;
+    background-size: cover;
+    color: FFFFFF;
+}
+.statistik {
+    left: reight;
+}
+</style>
